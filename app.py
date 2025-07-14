82d528fb33ac780b490189259d4b6249e58315c3
Christopher Petzold <cpetzold@waldronpw.com>
2025-07-14 16:53:01 -0400

    Add pie chart visualization for portfolio asset allocation comparison

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import matplotlib.ticker as mtick
 import pandas as pd
 
 from src.portfolio.portfolio_class import Portfolio
+from src.utils.charting import plot_pie
 from config.cme import capital_market_expectations, covariance_matrix, etf_proxies
 
 
@@ -18,7 +19,7 @@ page = st.sidebar.radio(
     [
         "Portfolio Analysis",
         "Monte Carlo Simulation",
-        "Historical Backtest",
+        # "Historical Backtest",
         "Stress Testing",
         "Fund Scoring"
     ]
@@ -91,29 +92,89 @@ if page == "Portfolio Analysis":
             cvar_prop = proposed_portfolio.conditional_value_at_risk(capital_market_expectations, covariance_matrix)
             max_dd_prop = proposed_portfolio.calculate_max_drawdown()
 
-            st. subheader("Portfolio Metrics")
-            col1, col2, = st.columns(2)
+            # Pie Charts
+            st.markdown("###")
+            st.divider()
+            st.markdown("###")
+            st.subheader("Asset Allocation Comparison")
+
+            labels = list(current_alloc.keys())
+            colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd']  # consistent asset class colors
+
+            # Create two pie charts side by side
+            fig, axes = plt.subplots(1, 2, figsize=(8, 4))
+
+            plot_pie(current_alloc, "Current Portfolio", ax=axes[0], colors=colors)
+            plot_pie(proposed_alloc, "Proposed Portfolio", ax=axes[1], colors=colors)
+
+            # Shared legend below
+            fig.legend(
+                labels,
+                loc="lower center",
+                bbox_to_anchor=(0.5, -0.15),
+                ncol=5,
+                frameon=False
+            )
+
+            plt.tight_layout()
+            st.pyplot(fig)
+
+            # col1, col2 = st.columns(2)
+            # with col1:
+            #     st.markdown("##### Current Portfolio")
+            #     fig1 = plot_pie(current_alloc, "Current Allocation")
+            #     st.pyplot(fig1)
+
+            # with col2:
+            #     st.markdown("##### Proposed Portfolio")
+            #     fig2 = plot_pie(proposed_alloc, "Proposed Allocation")
+            #     st.pyplot(fig2)
+            
+            st.markdown("###")
+            st.divider()
+            st.markdown("###")
+
+            st. subheader("Portfolio Return and Risk Comparison")
+            col1, col2, col3, col4, col5, col6, col7 = st.columns(7)
             with col1:
-                col1.metric("Expected Return", f"{exp_ret:.2%}" if exp_ret else "N/A")
-                col1.metric("Expected Volatility", f"{exp_std:.2%}" if exp_std else "N/A")
-                col1.metric("Sharpe Ratio", f"{sharpe_ratio:.2f}" if sharpe_ratio else "N/A")
-                col1.metric("CVaR", f"{cvar:.2%}" if cvar else "N/A")
-                col1.metric("Max Drawdown", f"{max_dd["max_drawdown"]:.2%}" if max_dd else "N/A")
-                col1.metric("Recovery", f"{max_dd["recovery_months"]:.0f} months" if max_dd else "N/A")
+                col1.metric(label="Allocation", value="Current")
+                col1.metric(label="", value="Proposed")
             
             with col2:
-                col2.metric("Expected Return", f"{exp_ret_prop:.2%}" if exp_ret_prop else "N/A")
-                col2.metric("Expected Volatility", f"{exp_std_prop:.2%}" if exp_std_prop else "N/A")
-                col2.metric("Sharpe Ratio", f"{sharpe_ratio_prop:.2f}" if sharpe_ratio_prop else "N/A")
-                col2.metric("CVaR", f"{cvar_prop:.2%}" if cvar_prop else "N/A")
-                col2.metric("Max Drawdown", f"{max_dd_prop["max_drawdown"]:.2%}" if max_dd_prop else "N/A")
-                col2.metric("Recovery", f"{max_dd_prop["recovery_months"]:.0f} months" if max_dd_prop else "N/A")
+                col2.metric("Expected Return", f"{exp_ret:.2%}" if exp_ret else "N/A")
+                # col2.metric("Expected Return", f"{exp_ret_prop:.2%}" if exp_ret_prop else "N/A")
+                col2.metric(label="", value=f"{exp_ret_prop:.2%}" if exp_ret_prop else "N/A")
+            
+            with col3:
+                col3.metric("Expected Volatility", f"{exp_std:.2%}" if exp_std else "N/A")
+                # col3.metric("Expected Volatility", f"{exp_std_prop:.2%}" if exp_std_prop else "N/A")
+                col3.metric(label="", value=f"{exp_std_prop:.2%}" if exp_std_prop else "N/A")
+            
+            with col4:
+                col4.metric("Sharpe Ratio", f"{sharpe_ratio:.2f}" if sharpe_ratio else "N/A")
+                # col4.metric("Sharpe Ratio", f"{sharpe_ratio_prop:.2f}" if sharpe_ratio_prop else "N/A")
+                col4.metric(label="", value=f"{sharpe_ratio_prop:.2f}" if sharpe_ratio_prop else "N/A")
+            
+            with col5:
+                col5.metric("CVaR", f"{cvar:.2%}" if cvar else "N/A")
+                # col5.metric("CVaR", f"{cvar_prop:.2%}" if cvar_prop else "N/A")
+                col5.metric(label="", value=f"{cvar_prop:.2%}" if cvar_prop else "N/A")
+            
+            with col6:
+                col6.metric("Max Drawdown", f"{max_dd["max_drawdown"]:.2%}" if max_dd else "N/A")
+                # col6.metric("Max Drawdown", f"{max_dd_prop["max_drawdown"]:.2%}" if max_dd_prop else "N/A")
+                col6.metric(label="", value=f"{max_dd_prop["max_drawdown"]:.2%}" if max_dd_prop else "N/A")
+            
+            with col7:
+                col7.metric("Recovery", f"{max_dd["recovery_months"]:.0f} months" if max_dd else "N/A")
+                # col7.metric("Recovery", f"{max_dd_prop["recovery_months"]:.0f} months" if max_dd_prop else "N/A")
+                col7.metric(label="", value=f"{max_dd_prop["recovery_months"]:.0f} months" if max_dd_prop else "N/A")
 
         st.markdown("###")
         st.divider()
         st.markdown("###")
-        st.subheader("📈 Growth of $10,000: Current vs Proposed Portfolio")
-        # Align and calculate cumulative performance
+
+        st.subheader("📈 Growth of $100,000: Current vs Proposed Portfolio")
         start_value = 100_000
         returns_df = (
             pd.DataFrame({
@@ -125,13 +186,9 @@ if page == "Portfolio Analysis":
 
         cumulative = (1 + returns_df).cumprod() * start_value
 
-        # Plot using matplotlib
         fig, ax = plt.subplots(figsize=(10, 4))
         cumulative.plot(ax=ax)
 
-        # ax.set_title(f"Growth of ${start_value:.0f} (2015–2025)")
-        # ax.set_ylabel("Portfolio Value ($)")
-        # ax.set_xlabel("Date")
         ax.yaxis.set_major_formatter(mtick.StrMethodFormatter('${x:,.0f}'))
         ax.spines["top"].set_visible(False)
         ax.spines["bottom"].set_visible(False)
@@ -149,7 +206,8 @@ if page == "Portfolio Analysis":
 
 if page == "Monte Carlo Simulation":
     st.title("Monte Carlo Simulation Tool for Goals Based Investing")
-    st.caption("Estimate an asset allocation's probability of meeting a client's goals using sophisticated statistics.")
+    # st.caption("Estimate an asset allocation's probability of meeting a client's goals using sophisticated statistics.")
+    st.caption("Please come back later. This section of the application is still in development.")
 
 if page == "Historical Backtest":
     st.caption("Please come back later. This section of the application is still in development.")
---
+++ src/utils/charting.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+
+def plot_pie(data: dict, title: str, ax=None, colors=None):
+    labels = list(data.keys())
+    sizes = list(data.values())
+
+    # Use passed-in axes or create a new one
+    if ax is None:
+        fig, ax = plt.subplots(figsize=(4, 4))
+    else:
+        fig = None  # don't return fig unless you create one
+
+    # Draw pie chart
+    wedges, _ = ax.pie(
+        sizes,
+        startangle=90,
+        colors=colors,
+        # wedgeprops=dict(width=0.6)
+    )
+
+    # Make it a circle and set title
+    ax.axis("equal")
+    ax.set_title(title, fontsize=12)
+
+    # Return fig only if created here
+    if fig:
+        return fig
